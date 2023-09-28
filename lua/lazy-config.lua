210a92f190cf86a63669d984f8a30461d5192792
Suchith Sridhar <suchithsridhar@gmail.com>
2023-09-27 22:11:34 -0300

    Updated to include luaSnip explicitly

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -66,6 +66,14 @@ require('lazy').setup({
     }
   },
 
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "2.*",
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
+  },
+
   -- Auto Pairs (Auto Close brackets/parenthesis)
   'jiangmiao/auto-pairs',
 
